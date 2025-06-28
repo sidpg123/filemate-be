144590cd9c4848169192336a868fb5ac3985d7b0
Siddharth <preparation013@gmail.com>
2025-06-28 23:35:51 +0530

    working on user features

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,14 +6,14 @@ import { ErrorHandler } from "../lib/utils.js";
 
 const isAuthenticated = TryCatch(async (req: AuthenticatedRequest, _res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
-    console.log("Authorization header:", authHeader);
+    // console.log("Authorization header:", authHeader);
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return next(new ErrorHandler("No token provided", 401));
   }
 
   const token = authHeader.split(" ")[1];
-  console.log("Token extracted:", token);
+  // console.log("Token extracted:", token);
   try {
     const decoded = jwt.verify(token, process.env.AUTH_SECRET!) as JwtPayload;
     if(decoded.id === undefined || typeof decoded.id !== 'string') {
@@ -26,7 +26,7 @@ const isAuthenticated = TryCatch(async (req: AuthenticatedRequest, _res: Respons
         req.user.id = decoded.id;
       }
     }
-    console.log("Decoded token:", decoded);
+    // console.log("Decoded token:", decoded);
 
     next();
   } catch (err) {
