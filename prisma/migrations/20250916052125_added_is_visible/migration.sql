33a7b645bf52fa95ca6e9b568d0e5fab2bb32506
Siddharth <siddharthpatilgurav@gmail.com>
2025-09-18 17:33:51 +0530

    prisma changes

---
+++ prisma/migrations/20250916052125_added_is_visible/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `visibleToAdmin` on the `UserDocument` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserDocument" DROP COLUMN "visibleToAdmin";
