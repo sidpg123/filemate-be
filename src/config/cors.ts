1bd84e515e0f148897b400f01849b83be71a3bc9
Siddharth <siddharthpatilgurav@gmail.com>
2025-10-07 00:25:18 +0530

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,7 @@ const corsOptions: cors.CorsOptions = {
     origin: [
       "http://localhost:3000",
       process.env.CLIENT_URL || 'http://localhost:3000',
+      'https://www.filesmate.in/'
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
